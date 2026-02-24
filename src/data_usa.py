e15669619d100adb5f92c5663f82ed1ca4aed39c
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:26:40 +0300

    Update data_usa.py

--- src/data_usa.py
+++ src/data_usa.py
@@ -1,10 +1,11 @@
-from requests import get
+from requests import Session
 
-class DataUSA:
+class DataUsa:
 	def __init__(self) -> None:
 		self.api = "https://datausa.io/api"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def get_data(
@@ -12,6 +13,5 @@ class DataUSA:
 			drill_downs: str,
 			measures: str,
 			year: str = "latest") -> dict:
-		return get(
-			f"{self.api}/data?drilldowns={drill_downs}&measures={measures}&year={year}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/data?drilldowns={drill_downs}&measures={measures}&year={year}").json()
